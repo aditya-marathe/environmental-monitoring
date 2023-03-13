f76275043f2f98f2da69f080e6bde97991b8a2f5
Super-meemaw <124065474+Super-meemaw@users.noreply.github.com>
2023-03-13 22:48:11 +0000

    Add files via upload

---
+++ EnviroPlus/CO2 compensation data transform.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+# Load the measured data
+measured_data = pd.read_csv('sensor_data.csv')
+
+# Load the calibration data
+calibration_data = pd.read_csv('Aranet4 042DF_2023-03-07T16_46_34+0000.csv')
+
+# Convert time strings to numerical format
+measured_data['timestamp'] = measured_data['timestamp'].apply(lambda x: datetime.strptime(x, r"%Y-%m-%d %H:%M:%S.%f").timestamp())
+calibration_data['Time'] = calibration_data['Time'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y %H:%M:%S').timestamp())
+
+# Interpolate the calibration data to match the time of the measured data
+calibration_data_interp = np.interp(measured_data['timestamp'], calibration_data['Time'], calibration_data['Carbon dioxide(ppm)'])
+
+# Calculate the correction factor as the mean ratio of measured to interpolated calibration data
+correction_factor = sum(calibration_data_interp) / sum(measured_data['co2_ppm'])
+
+# Apply the correction factor to the measured data
+calibrated_data = measured_data['co2_ppm'] * correction_factor
+
+# Save the calibrated data to a new file
+calibrated_data.to_csv('calibrated_data.csv', index=False)
+
+
+# In[24]:
+
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+# Load the measured data
+measured_data = pd.read_csv('sensor_data.csv')
+
+# Load the calibration data
+calibration_data = pd.read_csv('Aranet4 042DF_2023-03-07T16_46_34+0000.csv')
+
+# Convert time strings to numerical format and round to nearest second
+measured_data['timestamp'] = measured_data['timestamp'].apply(lambda x: round(datetime.strptime(x, r"%Y-%m-%d %H:%M:%S.%f").timestamp()))
+calibration_data['Time'] = calibration_data['Time'].apply(lambda x: round(datetime.strptime(x, '%d/%m/%Y %H:%M:%S').timestamp()))
+
+# Interpolate the calibration data to match the time of the measured data
+calibration_data_interp = np.interp(measured_data['timestamp'], calibration_data['Time'], calibration_data['Carbon dioxide(ppm)'])
+
+# Calculate the correction factor as the mean ratio of measured to interpolated calibration data
+correction_factor = np.mean(calibration_data_interp / measured_data['co2_ppm'])
+
+# Save the correction factor to a file
+np.savetxt('correction_factor.txt', [correction_factor])
+
+# Apply the correction factor to the measured data
+compensated_data = measured_data['co2_ppm'] * correction_factor
+
+# Save the compensated data to a new file
+compensated_data.to_csv('compensated_data.csv', index=False)
+
+print()
+
+
+# In[ ]:
+
+
+
+
